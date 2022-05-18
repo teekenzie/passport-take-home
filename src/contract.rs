77d4be67e0b67e35c9b4320b92bcf5fd5ebde80b
Kenzie Tee <teekenzie@berkeley.edu>
2022-05-17 20:32:43 -0700

    completed basic

--- src/contract.rs
+++ src/contract.rs
@@ -4,8 +4,9 @@ use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response,
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{State, STATE};
+use crate::msg::{ScoreResponse, OwnerResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{State, STATE, SCORES};
+use cosmwasm_std::Addr;
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:take-home";
@@ -16,10 +17,9 @@ pub fn instantiate(
     deps: DepsMut,
     _env: Env,
     info: MessageInfo,
-    msg: InstantiateMsg,
+    _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
     let state = State {
-        count: msg.count,
         owner: info.sender.clone(),
     };
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
@@ -27,8 +27,7 @@ pub fn instantiate(
 
     Ok(Response::new()
         .add_attribute("method", "instantiate")
-        .add_attribute("owner", info.sender)
-        .add_attribute("count", msg.count.to_string()))
+        .add_attribute("owner", info.sender))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -39,41 +38,40 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::Increment {} => try_increment(deps),
-        ExecuteMsg::Reset { count } => try_reset(deps, info, count),
+        ExecuteMsg::SetScore {entering_addr, score} => try_set_score(deps, info, entering_addr, score),
     }
 }
 
-pub fn try_increment(deps: DepsMut) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        state.count += 1;
-        Ok(state)
-    })?;
-
-    Ok(Response::new().add_attribute("method", "try_increment"))
-}
-
-pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
-    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
-        if info.sender != state.owner {
-            return Err(ContractError::Unauthorized {});
-        }
-        state.count = count;
-        Ok(state)
-    })?;
-    Ok(Response::new().add_attribute("method", "reset"))
+pub fn try_set_score(deps: DepsMut, info: MessageInfo, entering_addr: String, score: i32) -> Result<Response, ContractError> {
+    let entry_addr = deps.api.addr_validate(&entering_addr)?;
+    let state = STATE.load(deps.storage)?;
+    let update_score = |_| -> StdResult<i32> {
+        Ok(score)
+    };
+    if info.sender != state.owner {
+        return Err(ContractError:: Unauthorized {});
+    }
+    SCORES.update(deps.storage, &entry_addr, update_score)?;
+    Ok(Response::new().add_attribute("method", "set_score")) 
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
+        QueryMsg::GetOwner {} => to_binary(&query_owner(deps)?),
+        QueryMsg::GetScore {entering_addr} => to_binary(&query_score(deps, entering_addr)?),
     }
 }
 
-fn query_count(deps: Deps) -> StdResult<CountResponse> {
+fn query_owner(deps: Deps) -> StdResult<OwnerResponse> {
     let state = STATE.load(deps.storage)?;
-    Ok(CountResponse { count: state.count })
+    Ok(OwnerResponse { owner: state.owner }) 
+}
+
+fn query_score(deps: Deps, entering_addr: String) -> StdResult<ScoreResponse> {
+    let entry_addr = deps.api.addr_validate(&entering_addr)?;
+    let map_score = SCORES.load(deps.storage, &entry_addr)?;
+    Ok (ScoreResponse {score: map_score})
 }
 
 #[cfg(test)]
@@ -86,63 +84,71 @@ mod tests {
     fn proper_initialization() {
         let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
 
-        let msg = InstantiateMsg { count: 17 };
+        let msg = InstantiateMsg {};
         let info = mock_info("creator", &coins(1000, "earth"));
 
         // we can just call .unwrap() to assert this was a success
         let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
         assert_eq!(0, res.messages.len());
-
-        // it worked, let's query the state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
+        
+        // it worked, let's query the owner
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetOwner {}).unwrap();
+        let value: OwnerResponse = from_binary(&res).unwrap();
+        assert_eq!("creator", value.owner);
     }
 
     #[test]
-    fn increment() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
+    fn set_score() {
+        let mut deps = mock_dependencies_with_balance(&coins(2, "token")); 
 
-        let msg = InstantiateMsg { count: 17 };
+        let msg = InstantiateMsg {};
         let info = mock_info("creator", &coins(2, "token"));
         let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
 
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+        /* setting the score basic start */
+        let auth_info = mock_info("creator", &coins(2, "token"));
+        let msg = ExecuteMsg:: SetScore { entering_addr: "anyone".to_string(), score: 3}; 
+        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
 
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetScore { entering_addr: "anyone".to_string()}).unwrap();
+        let value: ScoreResponse = from_binary(&res).unwrap();
+        assert_eq!(3, value.score);
+        /* setting the score basic end */
 
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
+        /* unauthorized setting the score start */
         let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
+        let msg = ExecuteMsg::SetScore { entering_addr: "anyone".to_string(), score: 10 };
         let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
         match res {
             Err(ContractError::Unauthorized {}) => {}
             _ => panic!("Must return unauthorized error"),
         }
+        /* unauthorized setting the score end */
 
-        // only the original creator can reset the counter
+        /* setting the score of another address start */
         let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
+        let msg = ExecuteMsg:: SetScore { entering_addr: "anyone2".to_string(), score: 13}; 
         let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
 
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
-    }
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetScore { entering_addr: "anyone2".to_string()}).unwrap();
+        let value: ScoreResponse = from_binary(&res).unwrap();
+        assert_eq!(13, value.score);
+        /* setting the score of another address end */
+
+        /* setting the score of first address again start */
+        let auth_info = mock_info("creator", &coins(2, "token"));
+        let msg = ExecuteMsg:: SetScore { entering_addr: "anyone".to_string(), score: 25}; 
+        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
+
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetScore { entering_addr: "anyone".to_string()}).unwrap();
+        let value: ScoreResponse = from_binary(&res).unwrap();
+        assert_eq!(25, value.score);
+        /* setting the score of first address again end */
+
+        /* verify the second score untouched start */
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetScore { entering_addr: "anyone2".to_string()}).unwrap();
+        let value: ScoreResponse = from_binary(&res).unwrap();
+        assert_eq!(13, value.score);
+        /* verify the second score untouched end */
+    } 
 }
--- src/state.rs
+++ src/state.rs
@@ -2,12 +2,12 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
-    pub count: i32,
     pub owner: Addr,
 }
 
 pub const STATE: Item<State> = Item::new("state");
+pub const SCORES: Map<&Addr, i32> = Map::new("scores");
